9641685097271c6a9d34bd8a5c5d43c3c0ac18f6
Cyril Richard <cyril-romuald.richard@gfi.fr>
2020-04-14 16:53:59 +0200

    Modifs des chemins sur le buildcyrtools

--- BuildCyrTools.ps1
+++ BuildCyrTools.ps1
@@ -7,18 +7,18 @@ if ( -not (Test-Path $Module)){throw [System.Exception]::new("Module $($Module) 
 
 if ([string]::IsNullOrWhiteSpace($Platform))
 {
-    $envPath += ".local/share/powershell/Modules"
+    $envPath += "\WindowsPowerShell\Modules"
     Copy-Item -r $Module $envPath
 }
 
 if ($Platform.ToUpper() -eq 'UNIX')
 {
-    $envPath += ".local/share/powershell/Modules"
+    $envPath += "/.local/share/powershell/Modules"
     Copy-Item -r $Module $envPath
 }
 
 if ($Platform.ToUpper() -eq 'WIN32NT')
 {
-    $envPath += ".local/share/powershell/Modules"
+    $envPath += "\PowerShell\Modules"
     Copy-Item -r $Module $envPath
 }
