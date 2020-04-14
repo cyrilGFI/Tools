249e01b7b60f5118ef4436d74e44a4a5b665e070
Cyril Richard <cyril-romuald.richard@gfi.fr>
2020-04-14 17:12:07 +0200

    Modifs sur build

--- BuildCyrTools.ps1
+++ BuildCyrTools.ps1
@@ -1,6 +1,7 @@
-$envPath = [environment]::getfolderpath("mydocuments")
+﻿$envPath = [environment]::getfolderpath("mydocuments")
 $Platform = $PSVersionTable.Platform
 $Module = "CyrTools"
+$endMessage = "Module $($Module) Copié, lancer Import-Module $($Module)"
 
 $ErrorActionPreference = "Stop" 
 if ( -not (Test-Path $Module)){throw [System.Exception]::new("Module $($Module) non trouvé !!")}
@@ -8,19 +9,23 @@ if ( -not (Test-Path $Module)){throw [System.Exception]::new("Module $($Module) 
 if ([string]::IsNullOrWhiteSpace($Platform))
 {
     $envPath += "\WindowsPowerShell\Modules"
-    Copy-Item -r $Module $envPath
+    Copy-Item -r $Module $envPath -Force
+    Write-Warning $endMessage
+    return
 }
 
 if ($Platform.ToUpper() -eq 'UNIX')
 {
     $envPath += "/.local/share/powershell/Modules"
-    Copy-Item -r $Module $envPath
+    Copy-Item -r $Module $envPath -Force
+    Write-Warning $endMessage
+    return
 }
 
 if ($Platform.ToUpper() -eq 'WIN32NT')
 {
     $envPath += "\PowerShell\Modules"
-    Copy-Item -r $Module $envPath
+    Copy-Item -r $Module $envPath -Force
+    Write-Warning $endMessage
+    return
 }
-
-Write-Warning "Module $($Module) Copié, lancer Import-Module $($Module)"
